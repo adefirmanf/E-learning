216a06e2f2a1eedb38ac1ee08fc93119a8f7eec5
adefirmanf <syskeys32@gmail.com>
2020-10-04 20:13:19 +0700

    initial commit

---
+++ app/controllers/app.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Login() revel.Result {
+	return c.Render()
+}
+
+func (c App) Dashboard() revel.Result {
+	return c.Render()
+}
